0f9eea5000eaf650c865f5ee5233cbc7e6cb7e5d
chinmaykul-1 <chinmaykulkarni81@gmail.com>
2025-04-06 15:03:43 +0530

    added private subnets and routes

--- create_subnets.tf
+++ create_subnets.tf
@@ -18,12 +18,12 @@ resource "aws_route_table" "private_rt" {
 
 resource "aws_route_table_association" "private_association_1" {
     subnet_id = aws_subnet.private_subnet_1.id
-    route_table_id = aws_route_table.private_rt
+    route_table_id = aws_route_table.private_rt.id
   
 }
 
 resource "aws_route_table_association" "private_association_2" {
     subnet_id = aws_subnet.private_subnet_2.id
-    route_table_id = aws_route_table.private_rt
+    route_table_id = aws_route_table.private_rt.id
   
 }
