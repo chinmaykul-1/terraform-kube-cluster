2aed8146179ed5d20e1d3e2c36f0d1e7516543cd
chinmaykul-1 <chinmaykulkarni81@gmail.com>
2025-04-06 13:58:52 +0530

    added private subnets

--- create_subnets.tf
+++ create_subnets.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "private_subnet_1" {
     vpc_id = data.aws_vpc.default.id
-    cidr_block = "172.30.0.0/26"
+    cidr_block = "172.31.64.0/26"
     map_public_ip_on_launch = false
 }
 resource "aws_subnet" "private_subnet_2" {
     vpc_id = data.aws_vpc.default.id
-    cidr_block = "172.30.1.0/26"
+    cidr_block = "172.31.64.64/26"
     map_public_ip_on_launch = false
 }
